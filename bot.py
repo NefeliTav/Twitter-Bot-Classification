a0ff0616bd87af8ea4e89680ab96d0aa5e3474c9
NefeliTav <msstama2@yahoo.gr>
2021-05-07 21:28:32 +0300

    dataset almost clean

--- bot.py
+++ bot.py
@@ -1,17 +1,9 @@
-import requests
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pyspark
-from pyspark.sql import *
-from pyspark.sql.types import *
-from pyspark.sql.functions import *
-from pyspark import SparkContext, SparkConf
 import csv
 import tweepy
+import langdetect
 from tweepy import OAuthHandler
 
+# tweepy authentication
 consumer_key = "phCKVDVUS7nBmCvN5aJZWwrxo"
 consumer_secret = "3k7gMiVmxPPDI0C6kTc8uMTL0nSdNfeeU82OGcNVftkaMmujlR"
 access_token = "1389540894022545408-lovri9oSZKdLryO5JXqvfwuLeruEGq"
@@ -29,26 +21,70 @@ auth.get_access_token(user_pint_input)
 
 api = tweepy.API(auth)
 
-
-with open('twitter_human_bots_dataset_clean.csv', 'r', encoding='latin1') as inp, open('clean.csv', 'w', newline='') as out:
+# clean data and retrieve the interesting info for each user
+with open('twitter_human_bots_dataset.csv', 'r', encoding='latin1') as inp, open('clean_dataset.csv', 'w', newline='') as out:
     next(inp)
     writer = csv.writer(out)
-    writer.writerow(['id', 'name', 'screen name', 'location', 'description', 'followers',
-                    'friends', 'image', 'favourites', 'verified', 'tweets', 'account_type'])
+    writer.writerow(['id', 'account_type', 'screen_name', 'description', 'follower_count', 'geo_enabled', 'friends_count',  # fields to be taken into consideration
+                     'listed_count', 'created_at', 'statuses_count', 'has_extended_profile', 'default_profile',
+                     'default_profile_image', 'URL', 'retweets', 'with_url', 'with_mention', 'tweet_text'])
+
+    # check every user id
     for row in csv.reader(inp):
         try:
-            user = api.get_user(row[0])
+            # count
+            retweets = 0
+            with_mention = 0
+            with_url = 0
+            text = ""
 
-            tweets = api.user_timeline(screen_name=user.screen_name,
-                                       count=200,
-                                       include_rts=False,
-                                       tweet_mode='extended'
-                                       )
-            twenty_tweets = []
-            for info in tweets[:20]:
-                twenty_tweets.append(info.full_text)
-            writer.writerow([row[0], user.name, user.screen_name, user.location, user.description, user.followers_count,
-                            user.friends_count, user.default_profile_image, user.favourites_count, user.verified, twenty_tweets, row[1]])
+            # find user
+            user = api.get_user(row[0])
+            tweets = api.user_timeline(
+                screen_name=user.screen_name, count=130, include_rts=True, tweet_mode='extended')
+
+            # read 130 of user's tweets
+            for tweet in tweets:
+                try:
+                    if tweet.retweeted_status:
+                        retweets += 1
+                except AttributeError:
+                    pass
+                if tweet.entities['urls']:
+                    with_url += 1
+                if tweet.entities['user_mentions']:
+                    with_mention += 1
+
+                # combine all tweets into one big text
+                text = text + " " + tweet.full_text
+            text = text.replace("\n", " ")
+            if "expanded_url" not in user.entities.keys():
+                URL = "NO_URL"
+            else:
+                URL = user.entities["expanded_url"]
+
+            # find retweets,mentions and urls per tweet
+            if len(tweets) >= 1:
+                retweets = retweets / len(tweets)
+                with_mention = with_mention / len(tweets)
+                with_url = with_url/len(tweets)
+            else:
+                retweets = 0
+                with_url = 0
+                with_mention = 0
+
+            try:
+                # keep only english texts
+                if langdetect.detect(text) != 'en':
+                    continue
+            except langdetect.lang_detect_exception.LangDetectException:
+                continue
+
+            # create a new clean and complete csv file with the dataset
+            writer.writerow([row[0], row[1], user.screen_name, user.description.replace("\n", " "), user.followers_count,
+                             user.geo_enabled, user.friends_count, user.listed_count, user.created_at, user.statuses_count,
+                             user.has_extended_profile, user.default_profile, user.default_profile_image, URL, retweets, with_url,
+                             with_mention, text])
 
         except tweepy.TweepError:
             pass
