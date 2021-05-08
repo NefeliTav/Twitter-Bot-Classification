6e776135e2e0f4fff12a952b3b7f0537d045f95a
NefeliTav <msstama2@yahoo.gr>
2021-05-08 14:38:41 +0300

    organise dataset, remove urls,emojis,other languages

--- bot.py
+++ bot.py
@@ -6,13 +6,7 @@ import re
 
 
 def remove_emoji(string):
-    regrex_pattern = re.compile(pattern="["
-                                        u"\U0001F600-\U0001F64F"  # emoticons
-                                        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                                        u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                                        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                                        "]+", flags=re.UNICODE)
-    return regrex_pattern.sub(r'', text)
+    return string.encode('ascii', 'ignore').decode('ascii')
 
 
 # tweepy authentication
@@ -24,21 +18,14 @@ access_token_secret = "N0XU2cNl14QsO13IRDHCSFmuZFpELKqNG2pA9mkwaQfrg"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-"""
-redirect_url = auth.get_authorization_url()
-print(redirect_url)
-user_pint_input = input("What's the pin value?")
-auth.get_access_token(user_pint_input)
-"""
-
 api = tweepy.API(auth)
 
 # clean data and retrieve the interesting info for each user
-with open('twitter_human_bots_dataset.csv', 'r', encoding='latin1') as inp, open('clean_dataset.csv', 'w', newline='') as out:
+with open('twitter_human_bots_dataset.csv', 'r', encoding='latin1') as inp, open('twitter_human_bots_dataset_clean.csv', 'w', newline='') as out:
     next(inp)
     writer = csv.writer(out)
-    writer.writerow(['id', 'account_type', 'screen_name', 'description', 'follower_count', 'geo_enabled', 'verified', 'friends_count', 'listed_count',
-                    'created_at', 'statuses_count', 'has_extended_profile', 'default_profile', 'default_profile_image', 'retweets', 'with_url', 'with_mention', 'Tweet_Text'])
+    writer.writerow(['id', 'account_type', 'screen_name', 'follower_count', 'friends_count', 'listed_count', 'statuses_count', 'geo_enabled', 'verified',
+                    'created_at', 'has_extended_profile', 'default_profile', 'default_profile_image', 'retweets', 'with_url', 'with_mention', 'description', 'tweet_text'])
 
     # check every user id
     for row in csv.reader(inp):
@@ -52,7 +39,7 @@ with open('twitter_human_bots_dataset.csv', 'r', encoding='latin1') as inp, open
             # find user
             user = api.get_user(row[0])
             tweets = api.user_timeline(
-                screen_name=user.screen_name, count=130, include_rts=True, tweet_mode='extended')
+                screen_name=user.screen_name, count=130, include_rts=False, tweet_mode='extended')
 
             # read 130 of user's tweets
             for tweet in tweets:
@@ -69,6 +56,10 @@ with open('twitter_human_bots_dataset.csv', 'r', encoding='latin1') as inp, open
                 # combine all tweets into one big text
                 text = text + " " + tweet.full_text
             text = remove_emoji(text).replace("\n", " ")
+            text = re.sub(r"http\S+", "", text)
+            text = re.sub(
+                r"(?:\@|http?\://|https?\://|www)\S+", "", text)
+            text = " ".join(text.split())
 
             # find retweets,mentions and urls per tweet
             if len(tweets) >= 1:
@@ -87,12 +78,12 @@ with open('twitter_human_bots_dataset.csv', 'r', encoding='latin1') as inp, open
             except langdetect.lang_detect_exception.LangDetectException:
                 continue
 
+            # clean description
+            description = " ".join((re.sub(
+                r"(?:\@|http?\://|https?\://|www)\S+", "", remove_emoji(user.description).replace("\n", " "))).split()),
             # create a new clean and complete csv file with the dataset
-            writer.writerow([row[0], row[1], user.screen_name, remove_emoji(user.description).replace("\n", " "), user.followers_count,
-                             user.geo_enabled, user.verified, user.friends_count, user.listed_count, user.created_at,
-                             user.statuses_count,
-                             user.has_extended_profile, user.default_profile, user.default_profile_image, retweets, with_url,
-                             with_mention, text])
+            writer.writerow([row[0], row[1], user.screen_name, user.followers_count, user.friends_count, user.listed_count, user.statuses_count, user.geo_enabled, user.verified,
+                            user.created_at, user.has_extended_profile, user.default_profile, user.default_profile_image, retweets, with_url, with_mention, description, text])
 
         except tweepy.TweepError:
             pass
